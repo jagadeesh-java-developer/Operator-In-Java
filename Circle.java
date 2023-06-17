fc6e930485812cc3ba9a572c52b4c00f286e11e8
jagadeesh-java-developer <jagadeesh.java.developer@gmail.com>
2023-06-17 19:56:25 +0530

    Circle in Operator

---
+++ Circle.java
@@ -0,0 +1,10 @@
+class Circle{
+	public static void main(String[] args){
+		double  r =  12;
+		System.out.println("The value of Radius : "+r);
+
+		double c = 3.142*r*r;
+		System.out.println("Area of the Circle : "+c);
+
+	}
+}
